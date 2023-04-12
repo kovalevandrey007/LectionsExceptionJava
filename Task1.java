59e6dbf58061a810733a09d58d0c9d93aa79c9fe
Андрей Ковалев <kovalevandrey007@gmail.com>
2023-04-12 20:37:02 +0200

    BeginSave

---
+++ Task1.java
@@ -0,0 +1,6 @@
+public class Task1 {
+    public static void main(String[] args) { ex1(); }
+    private static void ex1() {
+        
+    }
+}
---
+++ Task2.java
@@ -0,0 +1,6 @@
+public class Task2 {
+    public static void main(String[] args) { ex1(); }
+    private static void ex1() {
+
+    }
+}
